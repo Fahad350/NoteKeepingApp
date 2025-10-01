1384080708b725f9756657ec437834f4715021ea
Fahad350 <fahadhashmi350@gmail.com>
2025-10-02 00:53:35 +0500

    set backend url axios

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const BackendUrl = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 export default BackendUrl;
